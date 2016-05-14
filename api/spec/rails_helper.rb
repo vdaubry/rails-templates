84d7c70ad242563f7a72ea1c6adcfe70c59e5253
Vincent Daubry <vdaubry@gmail.com>
2016-05-14 12:15:24 +0200

    update sidekiq spec behavior

--- api/spec/rails_helper.rb
+++ api/spec/rails_helper.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'sidekiq/testing'
-Sidekiq::Testing.fake!
+Sidekiq::Testing.inline!
 
 Rails.logger.level = Logger::ERROR
 
